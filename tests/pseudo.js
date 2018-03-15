11b57779de0b4cd8c49bc60da367749711b5c50f
Stephan Hoyer <ste.hoyer@gmail.com>
2018-03-15 08:30:30 +0100

    Add some more tests

--- tests/pseudo.js
+++ tests/pseudo.js
@@ -14,4 +14,38 @@ o.spec('pseudo helper', () => {
     const y = yss.hover({ color: 'black' })
     o(y.style[':hover'].style).deepEquals({ color: 'black' })
   })
+
+  o('add hover style as key/value', () => {
+    const y = yss.hover('color', 'black')
+    o(y.style[':hover'].style).deepEquals({ color: 'black' })
+  })
+
+  o('add hover style as template string', () => {
+    const y = yss.hover`
+      color: black
+      background: blue
+    `
+    o(y.style[':hover'].style).deepEquals({ color: 'black', background: 'blue' })
+  })
+
+  o('overwrite hover styles', () => {
+    const y = yss.hover`color: black`.hover`color: red`
+    o(y.style[':hover'].style).deepEquals({ color: 'red' })
+  })
+
+  o('use in helper', () => {
+    yss.helper('hovercolor', (y, color) => {
+      y.hover('color', color)
+    })
+    const y = yss.hover`color: brick`.hovercolor('yellow')
+    o(y.style[':hover'].style).deepEquals({ color: 'yellow' })
+  })
+
+  o('use in helper, reference normal style', () => {
+    yss.helper('hoverdarken', y => {
+      y.hover('color', `dark${y.style.color}`)
+    })
+    const y = yss`color: green`.hoverdarken
+    o(y.style[':hover'].style).deepEquals({ color: 'darkgreen' })
+  })
 })
