b70bba42fd2c84daa6753f8e08d1add4db314c9e
Stephan Hoyer <ste.hoyer@gmail.com>
2020-07-28 11:51:01 +0200

    Rebuild after package update

--- yss.js
+++ yss.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.Yss = factory());
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Yss = factory());
 }(this, (function () { 'use strict';
 
   var KEBAB_REGEX = /[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g;
